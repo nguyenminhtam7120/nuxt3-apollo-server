53f10183d0abea0cab822e92e2b5c66842be0de3
Minh Tam Nguyen <79628262+nguyenminhtam7120@users.noreply.github.com>
2023-05-10 16:59:17 +0700

    up

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>List product:</h3>
+    <div v-for="(item, idx) in result?.products" :key="idx">
+      <p>{{ idx + 1 }}: {{ item.title }}</p>
+    </div>
+    <br>
+    <h3>Actions</h3>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const query = gql`
+query {
+  products {
+    title
+    description
+  }
+}
+`
+const { result } = useQuery(query);
+
+</script>
